1714c1b1482a0d5b6af22dd6424f026a6cb21356
FrK5E <FrK5Ewzb4XbBN1aWlGUZ@proton.me>
2024-11-24 21:46:03 +0100

    some progress

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 
+#include <boost/serialization/strong_typedef.hpp>
+#include <iostream>
 #include <map>
+#include <optional>
 #include <string>
 #include <variant>
-#include <optional>
 
 class A {};
 class B {
@@ -24,39 +26,59 @@ private:
   std::map<Type, MyDataType> data;
 };
 
-
-template <> void Container::storeParam<A>(const A & arg) { data[Type::A] = arg; };
+template <> void Container::storeParam<A>(const A &arg) {
+  data[Type::A] = arg;
+};
 template <> A Container::getParam<A>() { return std::get<A>(data.at(Type::A)); }
 
-template <> void Container::storeParam<B>(const B &arg) { data[Type::B] = arg; };
-template <> B Container::getParam<B>() { return std::get<B>(data.at(Type::B)); };
-
-class ID_A {};
-class ID_B {};
-
-enum class Level { 
-    LEVEL1, 
-    LEVEL2
+template <> void Container::storeParam<B>(const B &arg) {
+  data[Type::B] = arg;
+};
+template <> B Container::getParam<B>() {
+  return std::get<B>(data.at(Type::B));
 };
 
+BOOST_STRONG_TYPEDEF(int, ID_A);
+BOOST_STRONG_TYPEDEF(int, ID_B);
+
+enum class LevelId { LEVEL1, LEVEL2 };
+
 class Settings {
+public:
+  Settings() {
+    level_A_members[ID_A{1}];
+    level_B_members[ID_B{1}];
+    level_A_members[ID_A{1}].storeParam(B{"Hello world"});
+  };
 
-    Settings(){}
+  template <class R>
+  std::pair<std::optional<R>, LevelId> getParam(const ID_A &id) {
+    if (level_A_members.contains(id)) {
+      return {level_A_members[id].getParam<R>(), LevelId::LEVEL1};
+    }
+    return {{}, LevelId::LEVEL1};
+  };
 
-    template<class R> 
-    std::pair<std::optional<R>,Level>  getParam( const ID_A & id );
+  template <class R>
+  std::pair<std::optional<R>, LevelId> getParam(const ID_B &id) { 
+    if ( level_B_members.contains(id)) { 
+        return {level_B_members[id].getParam<R>(), LevelId::LEVEL2};
+    }
+    return {{}, LevelId::LEVEL2};
+  }
 
-    template<class R> 
-    std::pair<std::optional<R>,Level>  getParam( const ID_B & id );
+private:
+  std::map<ID_A, Container> level_A_members;
+  std::map<ID_B, Container> level_B_members;
 };
 
 int main(int argc, char *argv[]) {
 
-  Container c{};
+  Settings s{};
 
-  c.storeParam<B>(B{"HI there"});
+  auto value = s.getParam<B>(ID_A{1});
 
-  A param = c.getParam<A>();
+  std::cout << value.first.value().name << std::endl;
 
   return 0;
 }
